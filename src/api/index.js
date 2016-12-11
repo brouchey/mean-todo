ea2eb7304e57aa5b1ec4ce8efb676595fd7b8e3b
brouchey <benrouch@me.com>
2016-12-11 16:34:16 +0100

    move api logic to its own file, import mock datas

---
+++ src/api/index.js
@@ -0,0 +1,14 @@
+'use strict';
+var express = require('express');
+var todos = require('../../public/mock/todos.json');	// import mock todos
+var router = express.Router();
+
+router.get('/todos', function(req, res) {
+     res.json({todos: todos});	// get mock todos object
+});
+
+// TODO: Add POST route to create new entries
+// TODO: Add PUT route to update existing entries
+// TODO: Add DELETE route to delete entries
+
+module.exports = router;	// export router module
--- src/app.js
+++ src/app.js
@@ -1,20 +1,12 @@
 'use strict';
 var express = require('express');
+var router = require('./api');	// import router module
+
 var app = express();
 
 app.use('/', express.static('public'));
 
-var router = express.Router();
-
-router.get('/todos', function(req, res) {
-     res.json({todos:[]});	// return todos object
-});
-
-// TODO: Add POST route to create new entries
-// TODO: Add PUT route to update existing entries
-// TODO: Add DELETE route to delete entries
-
-app.use('/api', router);	// avoid confusion between api and public routes
+app.use('/api', router);	// avoid confusion between api and public routes, creates an alias
 
 app.listen(3000, function() {
      console.log("The server is running on port 3000 !");
