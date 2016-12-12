84ea727598b344778b861fd4ddac510b6cbf3bf1
brouchey <benrouch@me.com>
2016-12-12 16:32:32 +0100

    update saveTodos method to also use a PUT when todo is edited

--- app/scripts/services/data.js
+++ app/scripts/services/data.js
@@ -16,8 +16,14 @@ angular.module('todoListApp')
 		var queue = [];
 		todos.forEach(function(todo) {
 			var request;
-			if(!todo._id) {     // if our todo does not have an id
-				request = $http.post('/api/todos', todo)     // post a request
+			if(!todo._id) {     // if our todo does not have an ID
+				request = $http.post('/api/todos', todo)     // create it with POST
+			} else {     // if the todo has an ID
+				request = $http.put('/api/todos' + todo._id, todo)	// update it with PUT
+				.then(function(result) {
+					todo = result.data.todo;
+					return todo;
+				});
 			};
 			queue.push(request);     // push a request to the queue
 			return $q.all(queue).then(function(results) {     // promise
