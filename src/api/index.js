4ba76d72fab4c68f86e7aa4c013941e5ebd2a240
brouchey <benrouch@me.com>
2016-12-12 15:14:49 +0100

    replace mock datas with Todo model in GET routes

--- src/api/index.js
+++ src/api/index.js
@@ -1,10 +1,20 @@
 'use strict';
+
 var express = require('express');
-var todos = require('../../mock/todos.json');	// import mock todos
+var Todo = require('../models/todo');	// use our Todo model
+// var todos = require('../../mock/todos.json');	// import mock todos
+
 var router = express.Router();
 
 router.get('/todos', function(req, res) {
-     res.json({todos: todos});	// get and return mock todos object
+	// res.json({todos: todos});	// get and return mock todos object
+	// getting out todos
+	Todo.find({}, function(err, todos) {	// 1st: Object, 2nd: callback function
+		if(err) {
+			return res.status(500).json({message: err.message});
+		}
+		res.json({todos: todos});
+     });
 });
 
 // TODO: Add POST route to create new entries
