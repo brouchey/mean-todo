1c651aab8f5cd69e298887004c4227d4db04f0b3
brouchey <benrouch@me.com>
2016-12-12 16:23:41 +0100

    configure new routes to create and update todos in our app

--- app/scripts/services/data.js
+++ app/scripts/services/data.js
@@ -3,17 +3,27 @@
 var angular = require('angular');
 
 angular.module('todoListApp')
-.service('dataService', function($http) {
-  this.getTodos = function(cb) {
-    $http.get('/api/todos').then(cb);
-  };
+.service('dataService', function($http, $q) {
+	this.getTodos = function(cb) {
+		$http.get('/api/todos').then(cb);
+	};
   
-  this.deleteTodo = function(todo) {
-    console.log("I deleted the " + todo.name + " todo!");
-  };
+	this.deleteTodo = function(todo) {
+		console.log("I deleted the " + todo.name + " todo!");
+	};
   
-  this.saveTodos = function(todos) {
-    console.log("I saved " + todos.length + " todos!");
-  };
-  
+	this.saveTodos = function(todos) {
+		var queue = [];
+		todos.forEach(function(todo) {
+			var request;
+			if(!todo._id) {     // if our todo does not have an id
+				request = $http.post('/api/todos', todo)     // post a request
+			};
+			queue.push(request);     // push a request to the queue
+			return $q.all(queue).then(function(results) {     // promise
+			// runs each request in parallels
+				console.log("I saved " + todos.length + " todos!");
+			});
+		});
+	};
 });
